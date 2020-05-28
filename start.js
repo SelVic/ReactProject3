f77292dedc0cccd392324ef14b64de82117acc9a
SelVic <limelefthand@gmail.com>
2020-05-28 23:49:23 +0300

    123

--- start.js
+++ start.js
@@ -2,12 +2,12 @@ const http = require('http')
 const port = 3000
 const requestHandler = (request, response) => {
     console.log(request.url)
-    response.end('Hello Node.js Server!')
+    response.end('Hello Server!')
 }
 const server = http.createServer(requestHandler)
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.log('error', err)
     }
-    console.log(`server is listening on ${port}`)
+    console.log({port})
 })
