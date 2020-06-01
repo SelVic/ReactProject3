e2315db82d8aab95ae93a1870c0426610cbdf032
SelVic <limelefthand@gmail.com>
2020-06-01 16:38:40 +0300

    123

--- start.js
+++ start.js
@@ -1,8 +1,13 @@
 let express = require('express');
+
 let app = express();
 
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/styles', express.static(path.join(__dirname, 'css')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.get('/', function(req, res) {
-    res.sendFile('index.html',[{root:__dirname}]);
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000);
