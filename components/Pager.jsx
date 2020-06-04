88ef99a49d611605a76c6589b1e1bce7ea51c057
SelVic <limelefthand@gmail.com>
2020-06-05 02:57:11 +0300

    123

--- components/Pager.jsx
+++ components/Pager.jsx
@@ -1,17 +1,33 @@
 import React from "react";
+import {Link} from "@reach/router";
 
 const Pager = props => {
     let pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let maxVisible = 3;
     let current = 1;
     let firstPage = 1;
+    let sides = current +-2;
+
+    // let PrevPage;
+    // let NextPage;
+
+    //подумать над счетом середины
+    //окргуление?
+    if (current > sides)
+        firstPage = current - sides;
+    // else if ()
 
     for (let i=0; i < maxVisible; i++ ){
         //??
         pages.push()
     }
     return (
-        <div>123</div>
+        <div>
+            {/*PrevPage*/}
+            {
+                pages.map((p,i) => p)
+            }
+        </div>
     )
 }
 
