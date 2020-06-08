545c78966719d306072cee3161b09369e702c9e4
SelVic <limelefthand@gmail.com>
2020-06-08 20:49:59 +0300

    123

--- components/Pager.jsx
+++ components/Pager.jsx
@@ -1,33 +1,70 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "@reach/router";
+import {render} from 'react-dom';
 
-const Pager = props => {
-    let pages = [1,2,3,4,5,6,7,8,9,10];
-    let total = 10;
-    let current = 1;
-    let firstPage = 1;
-    let visible = 5;
-    let sides = Math.floor(visible / 2)
-    let PrevPage;
-    let NextPage;
-    if (firstPage ) {
-        visible = Math.abs(sides - firstPage) + 1;
+// const Pager = props => {
+//     let pages = [1,2,3,4,5,6,7,8,9,10];
+//     let total = 10;
+//     let current = 1;
+//     let firstPage = 1;
+//     let visible = 5;
+//     let sides = Math.floor(visible / 2)
+//
+//     let updateCurr = (newCurr) => {
+//         current=newCurr;
+//     }
+//
+//     // if (firstPage ) {
+//     //     visible = Math.abs(sides - firstPage) + 1;
+//     // }
+//
+//     // if (current > sides)
+//     //     firstPage = current - sides;
+//
+//     // for (let i=0; i < visible; i++ ){
+//     //     pages.push()
+//     // }
+//     return (
+//         <div>
+//             {
+//                 pages.map((p, i) => <span key={p} onClick={e => updateCurr(p)}><Link to='/'>{p}{"   "}</Link></span>)
+//             }
+//             <div>{current}</div>
+//         </div>
+//
+//
+//     )
+// }
+
+class Pager extends Component {
+    state = {
+        pages :[1,2,3,4,5,6,7,8,9,10],
+        total : 10,
+        current : 1,
+        firstPage : 1,
+        visible : 5
+    };
+
+    updateCurr = (page) => {
+        this.setState({current : page})
+    };
+
+
+    render() {
+        let curr = this.state.current;
+        let pages = this.state.pages;
+        return (
+            <div>
+                {
+                    pages.map((p, i) => <span key={p} value = {p} onClick={e => e.currentTarget.value}><Link to='/'>{p}{"   "}</Link></span>)
+                }
+                <div>{curr}</div>
+            </div>
+        )
     }
-
-    if (current > sides)
-        firstPage = current - sides;
-
-    // for (let i=0; i < visible; i++ ){
-    //     pages.push(i + firstPage)
-    // }
-    return (
-        <div>
-            {
-                pages.map((p, i) => <Link to='/'>{p}{<a onClick={current => i}></a>}{"   "}</Link>)
-            }
-        </div>
-
-    )
 }
+
+
+
 
 export default Pager
--- dist/script.js
+++ dist/script.js
@@ -98,37 +98,122 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _reach_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @reach/router */ "./node_modules/@reach/router/es/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_2__);
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function () { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
-var Pager = function Pager(props) {
-  var pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  var total = 10;
-  var current = 1;
-  var firstPage = 1;
-  var visible = 5;
-  var sides = Math.floor(visible / 2);
-  var PrevPage;
-  var NextPage;
+ // const Pager = props => {
+//     let pages = [1,2,3,4,5,6,7,8,9,10];
+//     let total = 10;
+//     let current = 1;
+//     let firstPage = 1;
+//     let visible = 5;
+//     let sides = Math.floor(visible / 2)
+//
+//     let updateCurr = (newCurr) => {
+//         current=newCurr;
+//     }
+//
+//     // if (firstPage ) {
+//     //     visible = Math.abs(sides - firstPage) + 1;
+//     // }
+//
+//     // if (current > sides)
+//     //     firstPage = current - sides;
+//
+//     // for (let i=0; i < visible; i++ ){
+//     //     pages.push()
+//     // }
+//     return (
+//         <div>
+//             {
+//                 pages.map((p, i) => <span key={p} onClick={e => updateCurr(p)}><Link to='/'>{p}{"   "}</Link></span>)
+//             }
+//             <div>{current}</div>
+//         </div>
+//
+//
+//     )
+// }
 
-  if (firstPage) {
-    visible = Math.abs(sides - firstPage) + 1;
+var Pager = /*#__PURE__*/function (_Component) {
+  _inherits(Pager, _Component);
+
+  var _super = _createSuper(Pager);
+
+  function Pager() {
+    var _this;
+
+    _classCallCheck(this, Pager);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _super.call.apply(_super, [this].concat(args));
+
+    _defineProperty(_assertThisInitialized(_this), "state", {
+      pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      total: 10,
+      current: 1,
+      firstPage: 1,
+      visible: 5
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "updateCurr", function (page) {
+      _this.setState({
+        current: page
+      });
+    });
+
+    return _this;
   }
 
-  if (current > sides) firstPage = current - sides; // for (let i=0; i < visible; i++ ){
-  //     pages.push(i + firstPage)
-  // }
+  _createClass(Pager, [{
+    key: "render",
+    value: function render() {
+      var curr = this.state.current;
+      var pages = this.state.pages;
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, pages.map(function (p, i) {
+        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+          key: p,
+          value: p,
+          onClick: function onClick(e) {
+            return e.currentTarget.value;
+          }
+        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_reach_router__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          to: "/"
+        }, p, "   "));
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, curr));
+    }
+  }]);
 
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, pages.map(function (p, i) {
-    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_reach_router__WEBPACK_IMPORTED_MODULE_1__["Link"], {
-      to: "/"
-    }, p, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
-      onClick: function onClick(current) {
-        return i;
-      }
-    }), "   ");
-  }));
-};
+  return Pager;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
 
 /* harmony default export */ __webpack_exports__["default"] = (Pager);
 
