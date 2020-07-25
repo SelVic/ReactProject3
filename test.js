9863bc490f052c91cf814fffeb54f203ae2f4918
SelVic <limelefthand@gmail.com>
2020-07-25 18:59:03 +0300

    123

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
-//перебор в степени n
+let list1 = [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
+];
 
-let a = [1,2,3,4,5];
-
-let b = 9;
+let a = list1.map(person => Object.assign(person, {greeting: "Hi "+ person.firstName + ", what do you like the most about " + person.language + "?"}))
 
 console.log(a)
+
+
+let list2 = [
+    { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+    { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+    { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];
+
+function isRubyComing(list) {
+    let a = list.forEach()
+}
