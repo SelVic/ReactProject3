f42bf66daa6a03d46506b9454f12b47a4d6ea621
SelVic <limelefthand@gmail.com>
2020-07-26 23:55:59 +0300

    123

--- test.js
+++ test.js
@@ -17,6 +17,6 @@ function getFirstPython(list1) {
 //     const dev = list.find(x => x.language === "Python")
 //     return dev ? `${dev.firstName}, ${dev.country}` : "There will be no Python developers"
 // }
-
+//
 
 console.log(getFirstPython(list1))
