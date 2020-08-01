c0f778c98a43c36402ef62d821f22523376b85de
SelVic <limelefthand@gmail.com>
2020-08-01 23:44:20 +0300

    123

--- pages/MainMoviePage.jsx
+++ pages/MainMoviePage.jsx
@@ -7,6 +7,7 @@ import {Item} from "../components/Item";
 import {Header} from "../components/Header";
 import {ProgressBar} from "../components/ProgressBar";
 
+//
 
 class MainMoviePage extends Component{
     state = {
