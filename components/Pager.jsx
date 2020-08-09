1564150692daefb6a5b53acde2962566a9e4aa91
SelVic <limelefthand@gmail.com>
2020-08-09 21:33:53 +0300

    123

--- components/Pager.jsx
+++ components/Pager.jsx
@@ -35,7 +35,6 @@ const Pager = props => {
 //                 break;
 //             }
 //             }
-    //
     /* domain.com/users/10/vasya?cat=1&page=2#hook */
     let LeftPageButton = first(pages) > 1;
     let RightPageButton = last(pages) < maxPages;
