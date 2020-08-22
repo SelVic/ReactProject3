7b03305438d1a56ae7ac66583078c76e3096b489
SelVic <limelefthand@gmail.com>
2020-08-22 20:18:27 +0300

    123

--- test.js
+++ test.js
@@ -12,9 +12,9 @@
 //     }
 //     return maxDiff;
 // }
-<script>
-    (miliseconds =>{
-    let currentTime = new Date().getTime();
-    while (currentTime + miliseconds >= new Date().getTime()) {}
-})(10000)
-</script>
+// <script>
+//     (miliseconds =>{
+//     let currentTime = new Date().getTime();
+//     while (currentTime + miliseconds >= new Date().getTime()) {}
+// })(10000)
+// </script>
