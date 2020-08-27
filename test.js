f9555ec74a4c9ea0eb22c45b75d88f428b9884d1
SelVic <limelefthand@gmail.com>
2020-08-27 21:19:54 +0300

    clean

--- test.js
+++ test.js
@@ -1,17 +1,4 @@
-// function maxDifference(arr) {
-//     let maxDiff = -1;
-//     let min = arr[0];
-//     for (let i = 0; i < arr.length; i++) {
-//         if (arr[i] > min && maxDiff < arr[i] - min) {
-//             maxDiff = arr[i] - min;
-//         }
-//
-//         if (arr[i] < min) {
-//             min = arr[i];
-//         }
-//     }
-//     return maxDiff;
-// }
+
 // <script>
 //     (miliseconds =>{
 //     let currentTime = new Date().getTime();
