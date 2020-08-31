fc57bb99f272a014e3114d7edf2a066ea82a645c
SelVic <limelefthand@gmail.com>
2020-08-31 23:50:49 +0300

    clean

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
 
-// <script>
-//     (miliseconds =>{
-//     let currentTime = new Date().getTime();
-//     while (currentTime + miliseconds >= new Date().getTime()) {}
-// })(10000)
-// </script>
